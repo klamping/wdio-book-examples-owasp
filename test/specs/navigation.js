fdcbced61ba532cb45eb67efaab155e84b5a8284
Kevin Lamping <klamping@gmail.com>
2019-05-09 22:13:26 -0500

    example 1: switch to using assert module

--- test/specs/navigation.js
+++ test/specs/navigation.js
@@ -1,19 +1,17 @@
+const assert = require('assert');
+
 describe('Main Navigation', function () {
     it('should take you to the login page', function () {
         // Load the page
-        browser.url('./')
+        browser.url('./');
 
         // Get the title of the homepage, should be 'OWASP Juice Shop'
-        if (browser.getTitle() !== 'OWASP Juice Shop') {
-            throw new Error('Title of the homepage should be "OWASP Juice Shop"')
-        }
+        assert.strictEqual(browser.getTitle(), 'OWASP Juice Shop', 'Title of the homepage should be correct');
 
         // Click the 'About Us' navigation link
-        $('button*=About Us').click()
+        $('button*=About Us').click();
 
         // Get the URL of the about page, should include 'about'
-        if (browser.getUrl() !== 'http://juice-shop.herokuapp.com/#/about') {
-            throw new Error('URL of about should be correct')
-        }
-    })
-})
+        assert.strictEqual(browser.getUrl(), 'http://juice-shop.herokuapp.com/#/about');
+    });
+});

2aab8cd4ada99e7e4818c38bc21feceba9e73cb7
Kevin Lamping <klamping@gmail.com>
2019-05-09 22:09:26 -0500

    example 1: switch to throwing errors

--- test/specs/navigation.js
+++ test/specs/navigation.js
@@ -4,12 +4,16 @@ describe('Main Navigation', function () {
         browser.url('./')
 
         // Get the title of the homepage, should be 'OWASP Juice Shop'
-        console.log(browser.getTitle())
+        if (browser.getTitle() !== 'OWASP Juice Shop') {
+            throw new Error('Title of the homepage should be "OWASP Juice Shop"')
+        }
 
         // Click the 'About Us' navigation link
         $('button*=About Us').click()
 
         // Get the URL of the about page, should include 'about'
-        console.log(browser.getUrl())
+        if (browser.getUrl() !== 'http://juice-shop.herokuapp.com/#/about') {
+            throw new Error('URL of about should be correct')
+        }
     })
 })

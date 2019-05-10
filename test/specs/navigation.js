6daf0679aaafc5c73594e9679186e594061b1408
Kevin Lamping <klamping@gmail.com>
2019-05-09 22:17:13 -0500

    example 1: add in chai

--- test/specs/navigation.js
+++ test/specs/navigation.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+const expect = require('chai').expect;
 
 describe('Main Navigation', function () {
     it('should take you to the login page', function () {
@@ -6,12 +6,12 @@ describe('Main Navigation', function () {
         browser.url('./');
 
         // Get the title of the homepage, should be 'OWASP Juice Shop'
-        assert.strictEqual(browser.getTitle(), 'OWASP Juice Shop', 'Title of the homepage should be correct');
+        expect(browser.getTitle()).to.equal('OWASP Juice Shop', 'Title of the homepage should be correct');
 
         // Click the 'About Us' navigation link
         $('button*=About Us').click();
 
         // Get the URL of the about page, should include 'about'
-        assert.strictEqual(browser.getUrl(), 'http://juice-shop.herokuapp.com/#/about');
+        expect(browser.getUrl()).to.include('/about');
     });
 });
